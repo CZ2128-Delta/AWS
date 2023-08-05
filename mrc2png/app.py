98516cfe401576c229015a33ce9dfc4864220e78
CZ2128-Delta <113656119+CZ2128-Delta@users.noreply.github.com>
2023-08-05 16:33:34 +0800

    initial commit

---
+++ mrc2png/app.py
@@ -0,0 +1,122 @@
+import json
+import os
+from pathlib import Path
+import urllib
+import boto3
+import mrcfile
+import numpy as np
+import imageio
+import shutil
+import time
+import base64
+from PIL import Image
+
+# You can reference EFS files by including your local mount path, and then
+# treat them like any other file. Local invokes may not work with this, however,
+# as the file/folders may not be present in the container.
+ROOT_EFS_PATH = Path('/mnt/lambda/')
+s3_client = boto3.client('s3')
+
+MAX_IMAGE_SIZE = 1000
+
+def auto_contrast(image, t_mean=150.0/255.0, t_sd=40.0/255.0):
+    image = (image - image.min()) / (image.max() - image.min())
+    mean = image.mean()
+    sq = image ** 2
+    temp = sq.mean()
+    sd = temp - mean ** 2
+    sd = np.sqrt(sd)
+
+    f = sd / t_sd
+
+    black = mean - t_mean * f 
+    white = mean + (1 - t_mean) * f
+    
+    new_image = np.clip(image, black, white)
+    new_image = (new_image - black) / (white - black)
+    return new_image
+
+def convert_image_to_base64(image_path):
+    with open(image_path, 'rb') as image_file:
+        base64_string = base64.b64encode(image_file.read()).decode('utf-8')
+    return base64_string
+
+def lambda_handler(event, context):
+    # Get bucket and key from event
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'])
+    mrc_name = Path(key)
+    print(bucket, key)
+
+    # First update on info.json in user's directory
+    user_root_path = mrc_name.parents[2]
+    proj_name = mrc_name.parents[1].stem
+    info_json = user_root_path.joinpath('info.json')
+
+    response = s3_client.get_object(Bucket=bucket, Key=str(info_json))
+    content = response['Body'].read().decode('utf-8')
+    info_dict = json.loads(content)
+
+    info_dict[proj_name]['progress'] = str(0)
+
+    json_str = json.dumps(info_dict, indent=4)
+    bytes_data = json_str.encode('utf-8')
+    s3_client.put_object(Bucket=bucket, Key=str(info_json), Body=bytes_data)
+
+    # Create EFS subroot path
+    subroot_path = ROOT_EFS_PATH.joinpath(mrc_name.stem) # "/mnt/lambda/${mrc_stem}"
+    if subroot_path.exists():
+        shutil.rmtree(subroot_path, ignore_errors=True)
+    os.makedirs(subroot_path, exist_ok=True)
+    os.makedirs(subroot_path.joinpath('frames')) # "/mnt/lambda/${mrc_stem}/frames"
+    print(subroot_path)
+    # Download mrc to EFS ( mounted on an EC2 instance )
+    s3_client.download_file(bucket, key, subroot_path.joinpath(f'{mrc_name.stem}.mrc'))
+
+    # Parse mrc into {num_views} pngs
+    base64_dict = {}
+    mrc_data = mrcfile.open(subroot_path.joinpath(f'{mrc_name.stem}.mrc')).data.astype(np.float32)
+    for i, frame in enumerate(mrc_data):
+        frame = (auto_contrast(frame)*255).astype(np.uint8)
+        # Downsample images if too large
+        frame = Image.fromarray(frame)
+        W, H = frame.size
+        if W > MAX_IMAGE_SIZE or H > MAX_IMAGE_SIZE:
+            if W > H:
+                new_W, new_H = MAX_IMAGE_SIZE, int(H * MAX_IMAGE_SIZE / W)
+            else:
+                new_W, new_H = int(W * MAX_IMAGE_SIZE / H), MAX_IMAGE_SIZE
+            frame = frame.resize((new_W, new_H), resample=Image.Resampling.LANCZOS)
+        # Save images
+        frame_path = subroot_path.joinpath(f'frames/{mrc_name.stem}_{str(i).zfill(5)}.png')
+        imageio.imsave(frame_path, frame)
+        # Convert png to base64
+        base64_string = convert_image_to_base64(frame_path)
+        base64_dict[f'{mrc_name.stem}_{str(i).zfill(5)}.png'] = base64_string
+        # Upload pngs from EFS to S3
+        bucket_key = f'{mrc_name.parent}/{mrc_name.stem}/{mrc_name.stem}_{str(i).zfill(5)}.png'
+        s3_client.upload_file(frame_path, bucket, bucket_key)
+        # Generate json for each png
+        png_dict = {}
+        png_dict['bucket_name'] = bucket
+        png_dict['user_name'] = user_root_path.stem
+        png_dict['project_name'] = proj_name
+        png_dict['file_name'] = f'{mrc_name.stem}_{str(i).zfill(5)}.png'
+        png_dict['encoded_png_data'] = base64_string
+        json_bucket_key = f'{mrc_name.parent}/{mrc_name.stem}/{mrc_name.stem}_{str(i).zfill(5)}.json'
+        s3_client.put_object(Body=json.dumps(png_dict, indent=4), Bucket=bucket, Key=json_bucket_key)
+    # Save base64 into mrc_name.json
+    s3_client.put_object(Body=json.dumps(base64_dict, indent=4), Bucket=bucket, Key=f'{mrc_name.parent}/{mrc_name.stem}.json')
+
+    # Second update on info.json in user's directory
+    response = s3_client.get_object(Bucket=bucket, Key=str(info_json))
+    content = response['Body'].read().decode('utf-8')
+    info_dict = json.loads(content)
+
+    info_dict[proj_name]['last_modify_time'] = str(int(time.time()))
+    info_dict[proj_name]['thumbnail'] = base64_dict[next(iter(base64_dict))]
+    info_dict[proj_name]['progress'] = str(1)
+
+    json_str = json.dumps(info_dict, indent=4)
+    bytes_data = json_str.encode('utf-8')
+    s3_client.put_object(Bucket=bucket, Key=str(info_json), Body=bytes_data)
